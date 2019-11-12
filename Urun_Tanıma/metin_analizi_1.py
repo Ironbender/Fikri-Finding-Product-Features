9ec5f8988705107d62d50372e95c3aa9cdccf0c8
Ironbender <muhammedoguzdemir@gmail.com>
2019-11-12 12:31:54 +0300

    Add files via upload

---
+++ Urun_Tanıma/metin_analizi_1.py
@@ -0,0 +1,54 @@
+"""
+modül : metin analizi ile ürünün özelliklerinin belirlenmesi ve sayısallaştırılması
+
+deneme : 1
+
+amaç : ürünün özelliklerinin, ürünü tanımlayan bir metin üzerinden belirlenmesi ve hedef müşterinin
+        ürünün özelliklerini kullanarak en geniş kümeden daralarak tam istenilen çözüm kümesine yaklaştırılması
+
+bu denemedi özellikler: ---
+
+istenilen özellikler : 1- hızlı çalışma -> 5 dk bandında modülün işlemi gerçekleştirmesi
+
+metin : Your Sonicare brush head and handle are the ultimate team. The ProtectiveClean 4100 is a gentle 
+        power toothbrush with a pressure sensor that protects teeth and gums from excess brushing pressure. 
+        The snap-on Optimal Plaque Control brush head features densely-packed high-quality bristles that 
+        gives you extra gentle brushing removing up to 7x more plaque vs. a manual power toothbrush. Brush 
+        heads wear out over time. Our brush head replacement reminder tracks how long you have been using your 
+        brush head and how hard you have been brushing. It will beep to inform you when to replace your brush 
+        head. The easy-start feature slowly increases the power of the brush as you get used to the Sonicare 
+        experience and the Smartimer encourages a full 2 minutes brushing time as recommended by your 
+        dental professional.The battery charge indicator light lets you know when it is time 
+        recharge; 2 week battery life.
+
+
+"""
+
+"""
+1- veri hazırlanır
+2- veri temizlenir
+3- ETL
+4- EDA
+
+5- cümledeki konuların ne olduğunun bulunması
+6- cümledeki konuların önem sırasına göre sıralanması
+
+7- cümledeki konuların olumlu-olumsuz durumunun ölçeklenmesi
+8- paragraf üzerinde işlemler yapılması
+9- tüm metin üzerinde analiz yapılıp + - özelliklerin çıkarılması
+
+
+"""
+
+
+import matplotlib
+import pyTourch
+
+
+
+
+
+
+
+
+
